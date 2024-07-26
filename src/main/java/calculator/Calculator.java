3a86d226df53b691983dcb1a6f35011d9b45b2e7
inseooo0 <calmandpassion@korea.ac.kr>
2024-07-26 12:29:43 +0900

    feat: 2-1

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,28 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
+    public List<Integer> resultList = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operator) throws Exception {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                } else {
+                    return num1 / num2;
+                }
+            default:
+                throw new RuntimeException("잘못된 연산자 기호입니다.");
+        }
+    }
 }
