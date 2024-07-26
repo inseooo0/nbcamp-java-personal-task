3f5ad964fde6bd0f51e82fbe4af47172b39a6914
inseooo0 <calmandpassion@korea.ac.kr>
2024-07-26 12:51:00 +0900

    feat: 2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,27 +11,45 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         do {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            System.out.print("연산 종류를 선택하세요(사칙연산:1, 원의 넓이:2): ");
+            switch (sc.nextInt()) {
+                case 1:
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    int num1 = sc.nextInt();
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    int num2 = sc.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    char operator = sc.next().charAt(0);
 
-            int result = calculator.calculate(num1, num2, operator);
-            System.out.println("결과: " + result);
+                    int result = calculator.calculate(num1, num2, operator);
+                    System.out.println("결과: " + result);
 
-            List<Integer> resultList = calculator.getResultList();
-            resultList.add(result);
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if (sc.next().equals("remove")) {
-                calculator.removeResult();
+                    List<Integer> resultList = calculator.getResultList();
+                    resultList.add(result);
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                    if (sc.next().equals("remove")) {
+                        calculator.removeResult();
+                    }
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    if (sc.next().equals("inquiry")) {
+                        calculator.inquiryResults();
+                    }
+                    break;
+                case 2:
+                    System.out.print("반지름을 입력하세요: ");
+                    int num = sc.nextInt();
+
+                    Double area = calculator.calculateCircleArea(num);
+                    System.out.println("결과: " + area);
+
+                    calculator.getCircleList().add(area);
+                    calculator.inquiryCircleResults();
+                    break;
+                default:
+                    throw new RuntimeException("잘못된 입력입니다.");
             }
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if (sc.next().equals("inquiry")) {
-                calculator.inquiryResults();
-            }
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
 
         } while (!sc.next().equals("exit"));
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,9 +6,12 @@ import java.util.List;
 public class Calculator {
 
     private List<Integer> resultList;
+    private List<Double> circleList;
+    public static final Double PI = 3.14; // 변하지 않는 상수(final)이고 Calculator 생성 없이 외부에서 접근 가능(static)
 
     public Calculator() {
         resultList = new ArrayList<>();
+        circleList = new ArrayList<>();
     }
 
     public int calculate(int num1, int num2, char operator) throws Exception {
@@ -30,6 +33,10 @@ public class Calculator {
         }
     }
 
+    public Double calculateCircleArea(int num) {
+        return num * num * PI;
+    }
+
     public List<Integer> getResultList() {
         return resultList;
     }
@@ -38,12 +45,26 @@ public class Calculator {
         this.resultList = resultList;
     }
 
+    public List<Double> getCircleList() {
+        return circleList;
+    }
+
+    public void setCircleList(List<Double> circleList) {
+        this.circleList = circleList;
+    }
+
     public void removeResult() {
         resultList.removeFirst();
     }
 
     public void inquiryResults() {
         for (Integer i : resultList) {
+            System.out.println("지난 연산 결과 = " + i);
+        }
+    }
+
+    public void inquiryCircleResults() {
+        for (Double i : circleList) {
             System.out.println("지난 연산 결과 = " + i);
         }
     }
