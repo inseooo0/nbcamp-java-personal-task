ded784a34db68d5d60f17d56eda4365fba7b36b9
inseooo0 <calmandpassion@korea.ac.kr>
2024-07-26 12:32:24 +0900

    feat: 2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,13 +1,11 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
-        List<Integer> resultList = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
+        Calculator calculator = new Calculator();
 
         Scanner sc = new Scanner(System.in);
 
@@ -20,36 +18,18 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
-            int result = 0;
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
-                    } else {
-                        result = num1 / num2;
-                    }
-                    break;
-            }
+            int result = calculator.calculate(num1, num2, operator);
             System.out.println("결과: " + result);
 
-            resultList.add(result);
+            calculator.resultList.add(result);
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             if (sc.next().equals("remove")) {
-                resultList.removeFirst();
+                calculator.resultList.removeFirst();
             }
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             if (sc.next().equals("inquiry")) {
-                for (Integer i : resultList) {
-                    System.out.println("지난 연산 결과= " + i);
+                for (Integer i : calculator.resultList) {
+                    System.out.println("지난 연산 결과 = " + i);
                 }
             }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
